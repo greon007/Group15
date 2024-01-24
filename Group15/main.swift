93de8953eb94d689e6fe83e6cc124bcd45c86e46
Leo <leocesar_alcordo@hotmail.com>
2024-01-23 21:07:36 -0500

    Games and Movie Items in Main added

--- Group15/main.swift
+++ Group15/main.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-print("Hello, World!")
+//Games Items
+let gameItem1 = Game(id: 123, title: "God Of War", price: 89.0, publisher: "Sony", isMultiplayer: false)
+let gameItem2 = Game(id: 124, title: "Back 4 Blood", price: 50.0, publisher: "Warner Bros Games", isMultiplayer: true)
 
-
-let item1 = Item(id: 123, title: "God Of War", price: 89.0)
-print(item1.info)
-print(item1.printReceipt(isRefund: true, amount: 123.0))
+//Movie Items
+let movieItem1 = Movie(id: 223, title: "Oppenheimer", price: 60.0, runningTime: 180)
+let movieItem2 = Movie(id: 224, title: "Barbie", price: 45.0, runningTime: 114)
