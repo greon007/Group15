9da67a376f1b19a8dc3179d57135a46fea76fd7f
Julius Dejon <juliusdejon@gmail.com>
2024-01-23 22:16:13 -0500

    Added customer

--- Group15/Customer.swift
+++ Group15/Customer.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+
+protocol CustomerProtocol {
+    var itemsList: [OwnedItem] { get set }
+    var balance: Double { get set }
+    func reloadAccount(amount: Double)
+    func useItem(id: Int, numMinutes: Int)
+}
+
+class Customer : CustomerProtocol {
+    var itemsList: [OwnedItem] = []
+    var balance: Double = 10.0
+    
+    
+    func reloadAccount(amount: Double) {
+        balance += amount
+    }
+    func useItem(id: Int, numMinutes: Int) {
+        for item in itemsList {
+            if(id == item.id) {
+                item.minutesUsed += numMinutes
+            }
+        }
+    }
+}
